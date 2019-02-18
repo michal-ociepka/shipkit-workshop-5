f8dd77f87f466c481c6257396bc3b4313cd62b11
Michal Ociepka <michal.ociepka@collibra.com>
2019-02-18 13:28:35 +0100

    Version 2.0.0

--- src/test/java/com/example/shipkitworkshop/BlaTest.java
+++ src/test/java/com/example/shipkitworkshop/BlaTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class MathUtilsTest {
+public class BlaTest {
 
     @Test
     public void shouldCreateTriangle() {
