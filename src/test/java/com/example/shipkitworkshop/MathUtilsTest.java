cbaf21bb06082b6d2fe1cfcbd3a3adbb2a0907d8
Michal Ociepka <michal.ociepka@collibra.com>
2019-02-18 13:24:49 +0100

    Version 2.0.0

--- src/test/java/com/example/shipkitworkshop/MathUtilsTest.java
+++ src/test/java/com/example/shipkitworkshop/MathUtilsTest.java
@@ -11,11 +11,13 @@ public class MathUtilsTest {
     public void shouldCreateTriangle() {
         assertTrue(MathUtils.isTriangle(3, 4, 5));
         assertTrue(MathUtils.isTriangle(5, 6, 7));
+
     }
 
     @Test
     public void shouldNotCreateTriangle() {
         assertFalse(MathUtils.isTriangle(3, 4, 10));
+        assertFalse(MathUtils.isTriangle(3, 4, 100));
     }
 
 }
